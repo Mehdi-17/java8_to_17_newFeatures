598abd4a8a99de0b5115c30c8a729d4cf003eadb
Mehdi-17 <mehdi.serra57@gmail.com>
2023-11-09 11:58:00 +0100

    Add demo for java 16 stream.toList()

--- src/main/java/com/activit/sfeir/ListFeatures/ListFeaturesTest.java
+++ src/main/java/com/activit/sfeir/ListFeatures/ListFeaturesTest.java
@@ -16,15 +16,19 @@ public class ListFeaturesTest {
         numbers.add("two");
         numbers.add("three");
 
-        //Java 9 new features : List.of(), creates an immutable list
+        //Java 9 new features : List.of(), creates an immutable list. Cannot contains null values
         List<String> numbers2 = List.of("one", "two", "three");
 
         //Java 9 new features : List.copyOf(), creates an immutable list
-        //Copy of creates a copy of the list passed as a parameter
+        //Copy of creates a copy of the list passed as a parameter. Cannot contains null values
         List<String> copyNumbers = List.copyOf(numbers);
         //copyOf on an immutable list, create a reference to the same object
         List<String> copyNumbers2 = List.copyOf(numbers2);
 
+        //Java 16 : toList() returns an immutable list and can be called directly on stream. Can contains null values
+        List<String> immutableNumbers = numbers.stream().toList();
+
+        Assertions.assertNotSame(numbers, immutableNumbers); // numbers != immutableNumbers
         Assertions.assertNotSame(numbers, copyNumbers); // numbers != copyNumbers
         Assertions.assertSame(numbers2, copyNumbers2); // numbers2 == copyNumbers2
     }
